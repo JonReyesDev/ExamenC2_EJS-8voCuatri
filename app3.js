8ca3009fd526a1913c830cd662199ed9f8555ef9
JONATHAN REYES <jonath.lt3@gmail.com>
2024-03-15 13:00:11 -0700

    Se corrigieron unos detalles en app3.js

--- app3.js
+++ app3.js
@@ -1,24 +1,22 @@
 import express from 'express';
-import http  from 'http';
 import { fileURLToPath } from 'url';
-import json from 'body-parser';
 import path from 'path';
-import misrutas from './router/index.js';
 
-const puerto = 80;
+import router from './router/index.js';
+
+const puerto = 80; 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Declarar la variable punto de inicio
+const app = express();
 
-const main = express();
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 
-// Declarar primer ruta por omision
-main.set("view engine","ejs")
-main.set(express.static(__dirname+'/public'));
-main.use(json.urlencoded({extended:true}));
-main.use(misrutas.router);
+app.use(router);
 
-main.listen(puerto, ()=>{
-    console.log("Se inicio el servidor por el puerto: " + puerto)
-})
+app.listen(puerto, () => {
+  console.log(`Servidor corriendo en http://localhost:${puerto}`);
+});
+
